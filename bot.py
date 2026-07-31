8eba28a32889d39653b1b7103b4053a7c77214c1
Xene.arg <tifosileo@gmail.com>
2026-07-31 20:21:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -286,13 +286,10 @@ async def queja(ctx, *, texto=None):
 async def on_ready():
     print(f'Inspectora en línea. Logueada como {bot.user}')
     try:
-        mi_servidor = discord.Object(id=1394371062111666182) 
-        bot.tree.copy_global_to(guild=mi_servidor)
-        sincronizados = await bot.tree.sync(guild=mi_servidor)
-        print(f"✅ ¡Éxito! Se sincronizaron {len(sincronizados)} comandos en el servidor local.")
+        sincronizados = await bot.tree.sync()
+        print(f"✅ ¡Éxito! Se sincronizaron {len(sincronizados)} comandos de aplicación.")
     except Exception as e:
-
-
+        print(f"⚠️ Falla burocrática al sincronizar comandos: {e}")
 @bot.tree.command(name="presente", description="Fichá tu lealtad diaria al Estado. Tenés 32hs de margen antes de perder la racha.")
 async def presente(interaction: discord.Interaction):
     usuario_id = str(interaction.user.id)
